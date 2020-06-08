b002aa08bc39865a8651a623e2981c5af2d3c85b
paddyyates7890 <64584807+paddyyates7890@users.noreply.github.com>
2020-06-08 13:20:50 +0100

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+package com.example.hello;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+    }
+}
+
+// this is the first java file program i have writen in this java application i have made a simple
+// hello world program and saved it as a java file to do this i went to file - project structure -
+// artifacts - + (in the top left ) - created the new artifact which saves in the out file
+// then i built the jar file in the build section by pressing build artifact and choosing the artifact i needed
+// - then i have made a new configuration by pressing shift twise and the enter to edit configuations
+// i have then made a new config for artifact by naming it adding the file path leaving everything else default setting
+// and then building the artifact again at the
+// in the bottom box
